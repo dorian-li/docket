7f434204bab6749671eedec493967bed3ce6586e
dorian-li <49279922+dorian-li@users.noreply.github.com>
2024-07-07 22:36:24 +0800

    :construction_worker: 更新仓库地址

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,5 @@
 param(
-    # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "dorian-li/docket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
